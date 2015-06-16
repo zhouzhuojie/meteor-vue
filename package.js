66163b6c4239ec3f85133a48f8f0d2f320cf3693
zhouzhuojie <zhouzhuojie@gmail.com>
2015-06-16 18:09:41 -0400

    Update to 0.12.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Vue for Meteor. It provides data-driven components with a simple and flexible API.",
-  version: "0.12.0",
+  version: "0.12.1",
   git: "https://github.com/zhouzhuojie/meteor-vue.git",
   name: "vue:vue"
 });
