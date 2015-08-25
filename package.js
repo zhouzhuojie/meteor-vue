b365f8f98bd8538f1a80d8b9ac9d3b30416345df
zhouzhuojie <zhouzhuojie@gmail.com>
2015-08-25 02:11:10 -0400

    Upgrade to 0.12.10

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Vue for Meteor. It provides data-driven components with a simple and flexible API.",
-  version: "0.12.9",
+  version: "0.12.10",
   git: "https://github.com/zhouzhuojie/meteor-vue.git",
   name: "vue:vue"
 });
