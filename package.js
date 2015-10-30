cf917e2f9b3083405ea074948c91a0556acf9971
Neri Junior <neri@nerijunior.com>
2015-10-30 14:25:14 -0200

    Fix #10 - Removed coffeescript

--- package.js
+++ package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  summary: "Vue for Meteor. It provides data-driven components with a simple and flexible API.",
-  version: "0.12.10",
-  git: "https://github.com/zhouzhuojie/meteor-vue.git",
-  name: "vue:vue"
+    summary: "Vue for Meteor. It provides data-driven components with a simple and flexible API.",
+    version: "0.12.10",
+    git: "https://github.com/zhouzhuojie/meteor-vue.git",
+    name: "vue:vue"
 });
 
-Package.onUse(function (api) {
-  api.versionsFrom('1.0');
-  api.use(['underscore@1.0.0', 'coffeescript@1.0.2']);
-  api.addFiles('lib/vue/dist/vue.js', 'client');
-  api.addFiles('lib/main.coffee', 'client');
-  api.export('Vue', 'client');
+Package.onUse(function(api) {
+    api.versionsFrom('1.0');
+    api.use(['underscore@1.0.0']);
+    api.addFiles('lib/vue/dist/vue.js', 'client');
+    api.addFiles('lib/main.js', 'client');
+    api.export('Vue', 'client');
 });
 
-Package.onTest(function (api){
-  api.use(['vue:vue', 'tinytest'], ['client']);
-  api.addFiles('test-mrt:vue.js', ['client']);
+Package.onTest(function(api) {
+    api.use(['vue:vue', 'tinytest'], ['client']);
+    api.addFiles('test-mrt:vue.js', ['client']);
 });
