0d1671d299dacf2b14fa97e27c669d3b474324e6
zhouzhuojie <zhouzhuojie@gmail.com>
2015-11-13 00:28:46 -0500

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Vue for Meteor. It provides data-driven components with a simple and flexible API.",
-    version: "1.0.7",
+    version: "1.0.8",
     git: "https://github.com/zhouzhuojie/meteor-vue.git",
     name: "vue:vue"
 });
